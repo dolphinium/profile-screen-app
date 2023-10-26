f88ebb017c5049d7cc2d5a4db4b95abd4f03afdb
dolphinium <dolphofficial@gmail.com>
2023-10-26 14:06:15 +0300

    Home screen layout completed. The problem with profile photo will be handled afterwards.

---
+++ lib/profile_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          _buildProfileImage(context),
+          _buildProfileDetails(context),
+          _buildActions(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileImage(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      child: Image.asset(
+        'assets/images/profile_photo.jpg',
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+
+  Widget _buildProfileDetails(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Yunus Emre KORKMAZ',
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.w600),
+          ),
+          _buildDetailsRow('Age', '23'),
+          _buildDetailsRow('Status', 'Single ;)')
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailsRow(String heading, String value) {
+    return Row(
+      children: <Widget>[
+        Text(
+          '$heading: ',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Text(value)
+      ],
+    );
+  }
+
+  Widget _buildActions(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        _buildIcon(Icons.heart_broken,"A broken heart"),
+        _buildIcon(Icons.favorite,'Nasty Girls🔥🔥'),
+        _buildIcon(Icons.directions_walk,'👨🏽‍🦼👨🏽‍🦼')
+      ],
+    );
+  }
+
+  Widget _buildIcon(IconData icon, String text){
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children:<Widget> [
+            Icon(icon, size:40),
+            Text(text)
+          ],
+        ),
+    );
+  }
+}
